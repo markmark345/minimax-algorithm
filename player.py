a2c106c3b100aaf818b643a89b3057fdff0cb8d9
markmark345 <natapatchara.anuroje@gmail.com>
2022-04-01 11:40:43 +0700

    create move player

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from msilib.schema import Class
 import random
+from turtle import pos
 
 class Human:
     def __init__(self, marker):
@@ -9,3 +10,15 @@ class Human:
     def move(self, gi):
         while True:
             position = input("Your turn human: ")
+
+            try:
+                position = int(position)
+            except:
+                position = -1
+
+            if position not in gi.get_free_positions():
+                print("Can't move plase retry.")
+            else:
+                break
+
+        gi.mark(self.marker, position)
